27a9c157898563ceba6a6de369e09b5ef68a223e
ArshdeepBrar300 <84167966+ArshdeepBrar300@users.noreply.github.com>
2024-02-11 10:54:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// Q1
+//create a string and try to  add number to it
+let dummy = "Arshdeep";
+dummy = dummy + 9;
+console.log(dummy + 5);
+//Answer-> number is typecasted to string and concatenated
+
+//Q2
+console.log(typeof dummy)
+//Answer: string
+
+//Q3
+const obj={
+  name:'Arshdeep',
+  age:22
+}
+// obj=25
+//error , anything declared as const  cant be reassigned /reinitialized
+
+//Q4
+//adding new key to object
+obj.gender='male'
+console.log(obj)
+
+const dict={
+  Happy: "Feeling or showing pleasure or contentment.",
+  Fast: "Moving quickly; able to move or do something       quickly.",
+ Bright:" Emitting a lot of light; vivid or colorful.",
+  Small: "Little in size, amount, or degree.",
+  Jump: "Propel oneself upward or forward into the air."
+}
